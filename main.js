fa98b2005ce094befb7dfcd18231f0e23dfd7d81
Tobias Borges <tobias.borgesguimaraes@gmail.com>
2024-11-07 23:36:57 -0300

    dia 07/11, adicionado cor dos botoes, bloqueio dos botoes após o clique, e aparecimento do botao de proximo (falta adicionar a funcionalidade do mesmo)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const perguntas = [
     {
         imagem: "\imagens\imagem-pergunta-1.jpg",
-        enunciado: "Carros autônomos fazem forte uso de inteligência artificial. Com diversas câmeras ao redor do carro, a IA deve fazer uma análise profunda da imagem para determinar se há obstruções na pista (como um pedestre, ciclista ou mesmo outro carro), ou se é seguro atravessar",
+        enunciado: "Carros autônomos fazem forte uso de inteligência artificial. Com diversas câmeras ao redor do carro, a IA deve fazer uma análise profunda da imagem para determinar se há obstruções na pista (como um pedestre, ciclista ou mesmo outro carro), ou se é seguro atravessar.",
         pergunta: "Qual é a cor do céu?",
         respostas: [
             { texto: "Azul", correto: true},
@@ -13,7 +13,7 @@ const perguntas = [
     },
     {
         imagem: "\imagens\imagem-pergunta-2.jpg",
-        enunciado: "Carros autônomos podem se comunicar com outros carros autônomos na estrada, desde que todos eles tenham conexão com a internet, isso possibilita que ambos tomem decisões conjuntas, sobre qual carro irá passar primeiro, e até mesmo evitando colisões entre ambos, já que todos os seus movimentos são coordenados",
+        enunciado: "Carros autônomos podem se comunicar com outros carros autônomos na estrada, desde que todos eles tenham conexão com a internet, isso possibilita que ambos tomem decisões conjuntas, sobre qual carro irá passar primeiro, e até mesmo evitando colisões entre ambos, já que todos os seus movimentos são coordenados.",
         pergunta: "Qual é a cor do céu?",
         respostas: [
             { texto: "Azul", correto: true},
@@ -67,16 +67,52 @@ function iniciarQuiz() {
 }
 
 function mostrarQuestao() {
-    let questaoAtual = perguntas[indiceQuestaoAtual];
-    imagemDaQuestao.innerHTML = questaoAtual.imagem;
-    enunciadoDaQuestao.innerHTML = questaoAtual.enunciado;
-    perguntaDaQuestao.innerHTML = questaoAtual.pergunta;
 
+    apagarQuestaoAnterior(); // Remove as repostas da questão anterior, para que as novas possam ser adicionadas
+
+    let questaoAtual = perguntas[indiceQuestaoAtual]; // Seleciona a questão 
+    imagemDaQuestao.src = `/imagens/imagem-pergunta-${indiceQuestaoAtual+1}.jpg`; // Mostra a imagem da respectiva questao
+    enunciadoDaQuestao.innerHTML = questaoAtual.enunciado; // Atualiza o enunciado da questão
+    perguntaDaQuestao.innerHTML = questaoAtual.pergunta; // Atualiza a pergunta da questão
+
+    // Adiciona os botões das respostas
     questaoAtual.respostas.forEach(resposta => {
-        const botao = document.createElement('botao');
+        const botao = document.createElement('button');
         botao.innerHTML = resposta.texto;
         botao.classList.add('botao');
         respostasDaQuestao.appendChild(botao);
-    })
+
+        if (resposta.correto) {
+            botao.dataset.correto = resposta.correto; // Passa o valor verdadeiro ou falso para o botão 
+        }
+        botao.addEventListener("click", selecionarResposta); // Evento do clique no botao da resposta
+    });
 }
+
+function apagarQuestaoAnterior() {
+    botaoProximo.style.display = 'none';
+    while (respostasDaQuestao.firstChild) {
+        respostasDaQuestao.removeChild(respostasDaQuestao.firstChild);
+    }
+}
+
+// Função recebe o evento, e verifica se o botão clicado é o correto ou o incorreto
+function selecionarResposta(e) { 
+    const botaoSelecionado = e.target;
+    
+    if (botaoSelecionado.dataset.correto === "true") {
+        botaoSelecionado.classList.add('correto');
+    } else {
+        botaoSelecionado.classList.add('incorreto');
+    }
+    Array.from(respostasDaQuestao.children).forEach(botao => {
+        if (botao.dataset.correto === "true") {
+            botao.classList.add("correto"); // Deixa a resposta correta verde (para o caso do usuario selecionar a resposta errado)
+        }
+        botao.disabled = true; // Desativa o botao para que não seja possível clicar em outros botões após selecionar uma resposta
+    });
+    botaoProximo.style.display = "block"; // Faz aparecer o botao de proximo, para que o usuário possa ir para a próxima pergunta do quiz
+}
+
+iniciarQuiz();
 
